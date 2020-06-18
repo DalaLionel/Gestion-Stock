60c3d7a681cbde628d8fdae613bb4350d79f019a
Lionel_Dala <dala.lionel@gmail.com>
2020-06-18 12:20:50 +0200

    Modifie la liste des produits
    Finalisation du template de la liste des produits.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -23,15 +23,30 @@ class HomeController extends AbstractController
     /**
      * ici on crée une uri test suivie d'un paramètre entre accolades {id}
      * le paramètre entre accolades est dynamique
-     * @Route ("/test/{id}", name="test")
+     * @Route ("/test", name="test")
      */
-    public function test($id, Request $request, SessionInterface $session)
+    public function test(EntityManagerInterface $em)
     {
-    return $this->json([
-        'id'=>$id,
-        'section'=>$request->query->get('section','profil'),
-        'session'=>$session->get('user','DefaultUser')
-    ]);
+        //Création d'une entité
+        $product=new Product();
+
+        $product
+            ->setName('Jeans')
+            ->setDescription('Un jean très swag')
+            ->setPrice(79.99)
+            ->setQuantity(50)
+        ;
+        //ici l'entité product n'est pas encore enregistrée en base de données
+        dump ($product);
+
+        //Emregistrement (insertion)
+        // 1 préparer à l'enregistrement d'une entité
+        $em->persist($product);
+        // 2 Exécuter les requêtes sql
+        $em->flush();
+        //une fois qu'on a fait flush on exécute ce que
+        //fonction de debug
+        dd($product);
     }
 
 
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,9 +22,16 @@ class ProductController extends AbstractController
     /**
      * @Route ("/product/list", name="liste_produits")
      */
-    public function liste()
+    public function liste(ProductRepository $repository)
     {
-        return $this->render('/product/list.html.twig');
+        //Avec findAll() on récupère tous les résulats contenus dans le repo
+        //$result = $repository->findAll();
+        /*Avec findBy(['clé'=>'valeur']) on récupère toutes les entités ayant une clé ayant la valeur fournie*/
+        //$result = $repository->findBy(['name'=>'jeans']);
+        /*Avec findOneBy(['clé'=>'valeur']) on trouve la première entité qui a la clé de la valeur qu'on veut*/
+        //$result = $repository->findOneBy(['name'=>'jeans']);
+        //$result = $repository->find(2);
+        return $this->render('/product/list.html.twig', ['product_list' => $repository->findAll()]);
     }
 
     /**
@@ -32,6 +40,7 @@ class ProductController extends AbstractController
     public function ajout()
     {
         return $this->render('/product/add.html.twig');
+
     }
 
     /**
